c05d5b3c97efb0dea50ef047afc33eea541adf95
Kristo Baricevic <kristo.baricevic@gmail.com>
2026-02-02 11:31:50 -0500

    add pdrome

---
+++ Rinse-Feb4/leetcode_style/lengthOfLongestPal.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  if (s.length < 2) return s;
+
+  let start = 0;
+  let maxLength = 1;
+
+  function expand(left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      const len = right - left + 1;
+      if (len > maxLength) {
+        maxLength = len;
+        start = left;
+      }
+      left--;
+      right++;
+    }
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    expand(i, i); // odd length
+    expand(i, i + 1); // even length
+  }
+
+  return s.slice(start, start + maxLength);
+};
